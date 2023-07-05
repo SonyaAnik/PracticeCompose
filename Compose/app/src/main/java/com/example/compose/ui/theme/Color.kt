7942e03aa688e2336af72dface6d8f37f0e1a590
SonyaAnik <sonya.2003.02.17.93@gmail.com>
2023-07-05 17:13:29 +0600

    Dota2

---
+++ Compose/app/src/main/java/com/example/compose/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.compose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF050B18)
+val White = Color(0xFFEEF2FB)
+val DarkGrey = Color(0xFF1F2430)
+val Yellow = Color(0xFFF4D144)
+val LightBlack = Color(0xFF1A1F29)
+val TotalBlack = Color(0xFF000000)
